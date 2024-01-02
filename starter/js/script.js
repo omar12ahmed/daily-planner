7153d439471822247cd93c6864e789df8481d64f
Omar Ibrahim <omaribrahim@Omars-MacBook-Air-2.local>
2024-01-02 16:54:26 +0000

    highlighted the current time block to green

--- starter/js/script.js
+++ starter/js/script.js
@@ -1,7 +1,16 @@
 
 const currentDate = dayjs();
+const currentHour= dayjs().format("hA")
 
 const formattedDate= currentDate.format("dddd, DD MMMM")
 console.log(formattedDate)
 
-$("#currentDay").text(formattedDate)
+$("#currentDay").text(formattedDate);
+
+$(".time-block").each(function(){
+    var timeBlock = $(this).find(".hour").text()
+
+    if(timeBlock===currentHour){
+        $(this).addClass("currentTime")
+    }
+})
