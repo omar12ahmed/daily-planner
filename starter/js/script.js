32871728a9da90c5e08c0c1b53a2f91f0d159999
Omar Ibrahim <omaribrahim@Omars-MacBook-Air-2.local>
2024-02-07 15:55:41 +0000

    color coded the block depending on the time

--- starter/js/script.js
+++ starter/js/script.js
@@ -1,16 +1,56 @@
 
 const currentDate = dayjs();
-const currentHour= dayjs().format("hA")
+
 
 const formattedDate= currentDate.format("dddd, DD MMMM")
 console.log(formattedDate)
 
 $("#currentDay").text(formattedDate);
 
-$(".time-block").each(function(){
-    var timeBlock = $(this).find(".hour").text()
 
-    if(timeBlock===currentHour){
-        $(this).addClass("currentTime")
-    }
+
+function displayTasks(){
+
+    $(".time-block").each(function (){
+       var textarea = $(this).find("textarea")
+       var time = textarea.attr("data-hour")
+       textarea.val(localStorage.getItem("hour-"+time)
+       || ""
+       )
+
+
+      
+    });
+
+
+}
+
+
+function highlightTimeBlock(){
+    const currentHour= parseInt(dayjs().format("HH"))
+    
+    $(".time-block").each(function (){
+        var timeBlock= parseInt($(this).find("textarea").attr("data-hour"));
+        // console.log(timeBlock)
+        console.log(timeBlock >currentHour)
+        if(timeBlock===currentHour){
+        
+            $(this).addClass("present")
+        }else if(timeBlock > currentHour ){
+            $(this).addClass("future")
+        }else{$(this).addClass("past")}
+    });
+}
+$(document).ready(function(){
+    highlightTimeBlock();
+displayTasks()
+})
+
+
+$(".saveBtn").on("click", function (){
+
+    var hour= $(this).attr("data-hour")
+    var textValue= $(this).parent().find("textarea").val()
+    localStorage.setItem("hour-"+ hour,textValue)
+ 
 })
